2230c60dfedf7838e1dbc57b345a895658aa34ee
{Prateek <{prateekasthana0912@gmail.com}>
2025-01-27 11:41:07 +0530

    checking

---
+++ PHP12(switch).php
@@ -0,0 +1,22 @@
+<?php
+$ch='k';
+switch($ch){
+    case 'a':
+        echo "Vowel";
+        break;
+    case 'e':
+        echo "Vowel";
+        break;
+    case 'i':
+        echo "Vowel";
+        break;
+    case 'o':
+        echo "Vowel";
+        break;
+    case 'u':
+        echo "Vowel";
+        break;
+    default:
+        echo "Consonant";
+}   
+?>
